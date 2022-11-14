77f2c2011d687a4e52238d45813218e47e10e2c1
RodrigoCosta <costardgg@gmail.com>
2022-11-14 00:20:07 -0300

    MongoClient

--- src/index.js
+++ src/index.js
@@ -11,6 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect().then(() => {
+  db = mongoClient.db("batePapoUol");
+});
+
 app.post("/participants", (req, res) => {});
 
 app.get("/participantes", (req, res) => {});
